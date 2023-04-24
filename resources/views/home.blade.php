d6dfdfae9dfff2c348695705063aa2db78766689
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-04-23 21:12:37 -0300

    update text to english

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         @if (Route::has('login'))
             <div class="row justify-content-end">
                 <div class="col">
-                    <h1 class="h1 fw-bold">Olá, {{ auth()->user()->name }}</h1>
+                    <h1 class="h1 fw-bold">{{ __('Hello') }}, {{ auth()->user()->name }}</h1>
                 </div>
                 @auth
                     <form action="{{ route('logout') }}" method="post" class="col-3 col-lg-1">
